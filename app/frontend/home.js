53cbdf29c0342cbe77341085bf8f1f24e16ba0ad
Vlad-Ștefan Harbuz <vlad@vladh.net>
2017-06-12 19:59:14 +0200

    Add functional test graph

--- app/frontend/home.js
+++ app/frontend/home.js
@@ -1,5 +1,44 @@
-import {DISTRICTS} from './common'
+import {DISTRICTS, SETTINGS} from './common'
+import TestGraph from './test-graph'
 import * as d3 from 'd3'
+import async from 'async'
 
-console.log(DISTRICTS)
-console.log(d3)
+function transformDistrictData(districtData) {
+  return districtData.reduce((newData, datum) => {
+    newData[datum['Name']] = datum
+    return newData
+  }, {})
+}
+
+function transformRow(row) {
+  const numberKeys = [
+    '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008',
+    '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017',
+  ]
+  numberKeys.forEach((key) => {
+    if (row[key].length == 0 || row[key] == '-') {
+      row[key] = null
+    } else {
+      row[key] = +row[key]
+    }
+  })
+  return row
+}
+
+function getDistrict(allData, district, done) {
+  d3.csv(SETTINGS.csvPath + district + '.csv', transformRow, (districtData) => {
+    allData[district] = transformDistrictData(districtData)
+    done(null, allData)
+  })
+}
+
+function getData(done) {
+  async.reduce(DISTRICTS, {}, getDistrict, done)
+}
+
+getData((err, allData) => {
+  let testGraph = new TestGraph({
+    allData,
+    selSvg: '#test-graph-svg',
+  })
+})
---
+++ app/frontend/test-graph.js
@@ -0,0 +1,80 @@
+import {AGE_GROUPS} from './common'
+import * as d3 from 'd3'
+
+export default class TestGraph {
+  constructor({allData, selSvg}) {
+    this.allData = allData
+
+    const elSvg = document.querySelector(selSvg)
+    const elSvgDims = elSvg.getBoundingClientRect()
+    const svg = d3.select(selSvg)
+    const margins = {
+      top: 50,
+      right: 50,
+      bottom: 50,
+      left: 50,
+    }
+
+    this.width = elSvgDims.width - margins.left - margins.right
+    this.height = elSvgDims.height - margins.top - margins.bottom
+    this.x = d3.scaleBand().rangeRound([0, this.width]).padding(0.1)
+    this.y = d3.scaleLinear().rangeRound([this.height, 0])
+    this.g = svg
+      .append('g')
+      .attr('transform', `translate(${margins.left}, ${margins.right})`)
+    this.graphData = []
+
+    this.makeGraphData()
+    this.draw()
+  }
+
+  makeGraphData() {
+    let district = 'altstadt-grossbasel'
+    let year = '2016'
+    this.graphData = AGE_GROUPS.map((ageGroup) => ({
+      ageGroup: ageGroup,
+      value: this.allData[district]['Alter: ' + ageGroup][year]
+    }))
+    this.updateAxes()
+  }
+
+  updateAxes() {
+    this.x.domain(this.graphData.map((d) => d.ageGroup))
+    this.y.domain([0, d3.max(this.graphData.map((d) => d.value))])
+  }
+
+  draw() {
+    this.g.selectAll('.axis.axis--x').remove()
+    this.g
+      .append('g')
+      .attr('class', 'axis axis--x')
+      .attr('transform', `translate(0, ${this.height})`)
+      .call(d3.axisBottom(this.x))
+
+    this.g.selectAll('.axis.axis--y').remove()
+    this.g
+      .append('g')
+      .attr('class', 'axis axis--y')
+      .call(d3.axisLeft(this.y).ticks(10))
+      .append('text')
+      .attr('transform', 'rotate(-90)')
+      .attr('y', 6)
+      .attr('dy', '0.7rem')
+      .attr('text-anchor', 'end')
+      .text('Age group')
+
+    let bars = this.g
+      .selectAll('.bar')
+      .data(this.graphData)
+
+    bars.exit().remove()
+    bars
+      .enter()
+      .append('rect')
+      .attr('class', 'bar')
+      .attr('x', (d) => this.x(d.ageGroup))
+      .attr('y', (d) => this.y(d.value))
+      .attr('width', this.x.bandwidth())
+      .attr('height', (d) => this.height - this.y(d.value))
+  }
+}
